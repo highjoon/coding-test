232574c1a5d2b07a48abc2a1eb09a68064546e1e
highjoon <yundosa2@gmail.com>
2022-01-17 18:44:29 +0900

    9012 괄호

---
+++ BOJ/9012.js
@@ -0,0 +1,32 @@
+// let fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const input = [
+  6,
+  "(())())",
+  "(((()())()",
+  "(()())((()))",
+  "((()()(()))(((())))()",
+  "()()()()(()()())()",
+  "(()((())()(",
+];
+
+const T = input.shift();
+const result = [];
+
+for (let x of input) {
+  let temp = [];
+  for (let j of x) {
+    if (j === "(") {
+      temp.push("(");
+    } else if (temp.length && j === ")") {
+      temp.pop();
+    } else {
+      temp.push(")");
+      break;
+    }
+  }
+  temp.length ? result.push("NO") : result.push("YES");
+}
+
+console.log(result.join("\n"));
