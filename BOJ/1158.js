59315adbe1eccfa8bd4feb843c9d1f9ced3d0ba4
highjoon <yundosa2@gmail.com>
2022-01-17 18:10:55 +0900

    1158 요세푸스 문제

---
+++ BOJ/1158.js
@@ -0,0 +1,22 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const N = input[0];
+const K = input[1];
+
+const array = Array.from({ length: N }, (v, i) => i + 1);
+let result = "<";
+
+while (array.length) {
+  for (let i = 0; i < K; i++) {
+    array.push(array.shift());
+  }
+
+  if (array.length === 1) {
+    result += `${array.pop()}>`;
+  } else {
+    result += `${array.pop()}, `;
+  }
+}
+
+console.log(result);
