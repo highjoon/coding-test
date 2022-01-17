276f97ea50d8bc11b7d3fd9b93bd300743857078
highjoon <yundosa2@gmail.com>
2022-01-18 06:17:23 +0900

    2164 카드2

---
+++ BOJ/2164.js
@@ -0,0 +1,63 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const n = Number(input[0]);
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this._size = 0;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this._size++;
+
+    return newNode;
+  }
+
+  getHead() {
+    return this.head.value;
+  }
+
+  removeHead() {
+    this.head = this.head.next;
+    this.head.prev = null;
+    this._size--;
+  }
+
+  getSize() {
+    return this._size;
+  }
+}
+
+const linkedList = new LinkedList();
+
+for (let i = 1; i <= n; i++) {
+  linkedList.add(i);
+}
+
+while (linkedList.getSize() > 1) {
+  linkedList.removeHead();
+  linkedList.add(linkedList.getHead());
+  linkedList.removeHead();
+}
+
+console.log(linkedList.getHead());
