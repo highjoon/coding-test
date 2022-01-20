4a4353a459b07a249dbeedb3b2c3e4638d970b47
highjoon <yundosa2@gmail.com>
2022-01-21 07:46:33 +0900

    10869 사칙연산

---
+++ BOJ/10869.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(parseInt(A / B));
+console.log(A % B);
