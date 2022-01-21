14a7ff29ec1679208a0e6b4ab0f975eab825617a
highjoon <yundosa2@gmail.com>
2022-01-21 13:23:27 +0900

    4344 평균은 넘겠지

--- BOJ/4344.js
+++ BOJ/4344.js
@@ -1,13 +1,5 @@
-// let fs = require("fs");
-// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-let input = [
-  "5",
-  "5 50 50 70 80 100",
-  "7 100 95 90 80 70 60 50",
-  "3 70 90 80",
-  "3 70 90 81",
-  "9 100 99 98 97 96 95 94 93 91",
-];
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const C = Number(input[0]);
 
