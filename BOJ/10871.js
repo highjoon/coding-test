614613cf9078238557c6e1cf7930bf1caecfd5e0
highjoon <yundosa2@gmail.com>
2022-01-21 09:49:38 +0900

    10871 X보다 작은 수

---
+++ BOJ/10871.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = input[0].split(" ")[0];
+const X = input[0].split(" ")[1];
+const arr = input[1].split(" ").map((v) => Number(v));
+let result = "";
+
+for (let i = 0; i < N; i++) {
+  if (arr[i] < X) {
+    result += `${arr[i]} `;
+  }
+}
+
+console.log(result);
