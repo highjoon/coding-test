3baf967377e394218af8cdaa81adf3e9013aed80
highjoon <yundosa2@gmail.com>
2022-01-21 09:34:07 +0900

    2884 알람 시계

---
+++ BOJ/2884.js
@@ -0,0 +1,28 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  let input = line.split(" ");
+
+  let H = Number(input[0]);
+  let M = Number(input[1]);
+
+  if (M - 45 < 0) {
+    M = 60 + (M - 45);
+    H -= 1;
+
+    if (H === -1) {
+      H = 23;
+    }
+  } else {
+    M -= 45;
+  }
+
+  console.log(H + " " + M);
+}).on("close", function () {
+  process.exit();
+});
