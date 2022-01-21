e43ba085587e48b98f3045f7cf0f4852463a02f7
highjoon <yundosa2@gmail.com>
2022-01-21 12:12:56 +0900

    2577 숫자의 개수

---
+++ BOJ/2577.js
@@ -0,0 +1,20 @@
+// let fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let input = ["150", "266", "427"];
+const A = Number(input[0]);
+const B = Number(input[1]);
+const C = Number(input[2]);
+let sum = A * B * C;
+sum = sum.toString();
+
+let obj = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 };
+
+for (let str of sum) {
+  const tempNum = Number(str);
+  obj[tempNum]++;
+}
+
+for (let x in obj) {
+  console.log(obj[x]);
+}
