d2150ac68fdffaf2a0ef8e08f7b6e80bf424460d
highjoon <yundosa2@gmail.com>
2022-01-21 12:21:15 +0900

    3052 나머지

--- BOJ/3052.js
+++ BOJ/3052.js
@@ -1,9 +1,5 @@
-// let fs = require("fs");
-// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-
-// let input = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-// let input = ["42", "84", "252", "420", "840", "126", "42", "84", "420", "126"];
-let input = ["39", "40", "41", "42", "43", "44", "82", "83", "84", "85"];
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let arr = input.map((v) => {
   return Number(v) % 42;
