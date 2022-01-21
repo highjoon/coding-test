bb98a9a69025e192cf901d60308eadb1bdc8e95f
highjoon <yundosa2@gmail.com>
2022-01-21 10:10:13 +0900

    11021 A+B - 7

---
+++ BOJ/11021.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const T = Number(input.shift());
+let index = 1;
+
+for (let x of input) {
+  const A = Number(x.split(" ")[0]);
+  const B = Number(x.split(" ")[1]);
+  const C = A + B;
+  console.log(`Case #${index++}: ${C}`);
+}
