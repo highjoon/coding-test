74881dae234cdcf77ca1c3c1d52c352f90a58e9e
highjoon <yundosa2@gmail.com>
2022-01-21 10:17:57 +0900

    8393 합

---
+++ BOJ/8393.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  let num = Number(line);
+  let sum = 0;
+
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+
+  console.log(sum);
+}).on("close", function () {
+  process.exit();
+});
