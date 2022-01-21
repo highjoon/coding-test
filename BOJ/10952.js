5ab6afc02ae02d3c24ed0bcdd14e7feac73b2b38
highjoon <yundosa2@gmail.com>
2022-01-21 10:20:24 +0900

    10952 A+B - 5

---
+++ BOJ/10952.js
@@ -0,0 +1,24 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let inputs = [];
+let isNotZero = true;
+let i = 0;
+let result = "";
+
+while (isNotZero) {
+  if (i !== 0) {
+    result += "\n";
+  }
+
+  inputs.push(input[i].split(" "));
+  let sum = Number(inputs[i][0]) + Number(inputs[i][1]);
+  if (sum === 0) {
+    isNotZero = false;
+  } else {
+    result += sum.toString();
+    i++;
+  }
+}
+
+console.log(result);
