5524cba1ead604c836d7d3407cab1c3cffc20020
highjoon <yundosa2@gmail.com>
2022-01-23 23:30:58 +0900

    11720 숫자의 합

---
+++ BOJ/11720.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let N = Number(input[0]);
+let num = input[1].split("");
+let sum = 0;
+
+for (let i = 0; i < N; i++) {
+  sum += Number(num[i]);
+}
+
+console.log(sum);
