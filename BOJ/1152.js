101aa73107ad20ab52395a4d8df7edeb9b400d64
highjoon <yundosa2@gmail.com>
2022-01-24 13:39:04 +0900

    1152 단어의 개수

---
+++ BOJ/1152.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const words = input.split(" ");
+let cnt = 0;
+
+for (let word of words) {
+  if (word) {
+    cnt++;
+  }
+}
+
+console.log(cnt);
