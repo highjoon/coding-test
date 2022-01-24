c431aebd7dfe0658d13727cfc65627f251fb19a0
highjoon <yundosa2@gmail.com>
2022-01-25 02:01:32 +0900

    10870 피보나치 수 5

---
+++ BOJ/10870.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let n = Number(input);
+
+function fibonacci(num) {
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
+
+console.log(fibonacci(n));
