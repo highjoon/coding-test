0d165e77e4fcf0939cac45ad04bc95e6f02dae90
highjoon <yundosa2@gmail.com>
2022-01-25 01:24:10 +0900

    1085 직사각형에서 탈출

---
+++ BOJ/1085.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((num) => parseInt(num));
+
+const x = input[0];
+const y = input[1];
+const w = input[2];
+const h = input[3];
+const answer = [x, y, w - x, h - y];
+
+console.log(Math.min(...answer));
