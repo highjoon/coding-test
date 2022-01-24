2596cd8e00d6747ede54895670882c87401f89cc
highjoon <yundosa2@gmail.com>
2022-01-24 21:51:30 +0900

    2775 부녀회장이 될테야

---
+++ BOJ/2775.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const T = Number(input.shift());
+
+for (let i = 0; i < T; i++) {
+  const k = +input.shift();
+  const n = +input.shift();
+
+  const apt = Array.from(Array(k + 1), () => Array(n + 1).fill(0));
+
+  for (let i = 1; i <= n; i++) {
+    apt[0][i] = i;
+  }
+
+  for (let i = 1; i <= k; i++) {
+    for (let j = 1; j <= n; j++) {
+      apt[i][j] = apt[i - 1][j] + apt[i][j - 1];
+    }
+  }
+
+  console.log(apt[k][n]);
+}
