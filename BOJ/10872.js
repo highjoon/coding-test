dfc07d7deed369dc2c6d6fcfef1a99e8cc69a14b
highjoon <yundosa2@gmail.com>
2022-01-25 01:57:04 +0900

    10872 팩토리얼

---
+++ BOJ/10872.js
@@ -0,0 +1,14 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let N = Number(input);
+
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(N !== 0 ? factorial(N) : 1);
