ec79f568c8c4c470723a255af22f93d002c75043
highjoon <yundosa2@gmail.com>
2022-01-25 11:28:25 +0900

    2447 별 찍기 - 10

---
+++ BOJ/2447.js
@@ -0,0 +1,27 @@
+// let fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().trim();
+let input = "27";
+let N = Number(input);
+
+let result = [];
+
+const star = (i, j, num) => {
+  if (i % 3 === 1 && j % 3 === 1) {
+    result.push(" ");
+  } else {
+    if (num === 1) {
+      result.push("*");
+    } else {
+      star(parseInt(i / 3), parseInt(j / 3), parseInt(num / 3));
+    }
+  }
+};
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    star(i, j, N);
+  }
+  result.push("\n");
+}
+
+console.log(result.join(""));
