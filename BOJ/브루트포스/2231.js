9ba2ca983285c3375023934722a7cb263b37f627
highjoon <yundosa2@gmail.com>
2022-01-26 12:17:53 +0900

    2231 분해합

---
+++ BOJ/브루트포스/2231.js
@@ -0,0 +1,20 @@
+const num = parseInt(require("fs").readFileSync("/dev/stdin").toString());
+
+let start = num - (num + "").length * 9;
+let answer = 0;
+if (start < 0) start = 0;
+for (let i = start; i < num; i++) {
+  if (
+    i
+      .toString()
+      .split("")
+      .map((n) => parseInt(n))
+      .reduce((acc, n) => acc + n, 0) +
+      i ===
+    num
+  ) {
+    answer = i;
+    break;
+  }
+}
+console.log(answer);
