930dac94312b1cc89fd20cdaaffa73a73d93eef6
highjoon <yundosa2@gmail.com>
2022-02-01 10:09:01 +0900

    11650 좌표 정렬하기

---
+++ BOJ/정렬/11650.js
@@ -0,0 +1,21 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = Number(input.shift());
+
+let answer = "";
+
+let arr = input
+  .map((v) => v.split(" ").map((x) => Number(x)))
+  .sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    } else {
+      return a[0] - b[0];
+    }
+  })
+  .forEach((x) => {
+    answer += `${x[0]} ${x[1]}\n`;
+  });
+
+console.log(answer);
