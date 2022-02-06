5fe8cf49910c2dea3a39ee36b1ab4952793a2ded
highjoon <yundosa2@gmail.com>
2022-02-06 18:32:08 +0900

    10773 제로

---
+++ BOJ/스택/10773.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const K = Number(input.shift());
+const arr = input.map((v) => Number(v));
+const stack = [];
+
+for (let i = 0; i < K; i++) {
+  if (arr[i] === 0) {
+    stack.pop();
+  } else {
+    stack.push(arr[i]);
+  }
+}
+
+stack.length ? console.log(stack.reduce((a, b) => a + b)) : console.log(0);
