2f602f4f563aafba05e8543c3dac870ff917545c
highjoon <yundosa2@gmail.com>
2022-02-06 21:24:18 +0900

    4949 균형잡힌 세상

---
+++ BOJ/스택/4949.js
@@ -0,0 +1,38 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+input.pop();
+
+const open = ["(", "["];
+const close = [")", "]"];
+const answer = [];
+
+for (let str of input) {
+  const stack = [];
+  let isBalanced = true;
+
+  for (let word of str) {
+    if (open.includes(word)) {
+      stack.push(word);
+    } else if (close.includes(word)) {
+      const lastWord = stack.pop();
+
+      if (lastWord !== open[close.indexOf(word)]) {
+        isBalanced = false;
+        break;
+      }
+    }
+  }
+
+  if (!isBalanced) {
+    answer.push("no");
+  } else {
+    if (stack.length === 0) {
+      answer.push("yes");
+    } else {
+      answer.push("no");
+    }
+  }
+}
+
+console.log(answer.join("\n"));
