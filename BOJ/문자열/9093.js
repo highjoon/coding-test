59fb2a53a3a6c9ca806b91b74c6221bba5c9c4e7
highjoon <yundosa2@gmail.com>
2022-02-09 01:33:58 +0900

    9093 단어 뒤집기

---
+++ BOJ/문자열/9093.js
@@ -0,0 +1,19 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const T = Number(input.shift());
+const answer = [];
+
+for (let sentence of input) {
+  const result = [];
+  const splited = sentence.split(" ");
+
+  for (let word of splited) {
+    const reversed = word.split("").reverse().join("");
+    result.push(reversed);
+  }
+
+  answer.push(result.join(" "));
+}
+
+console.log(answer.join("\n"));
