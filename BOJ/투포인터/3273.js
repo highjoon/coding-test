c89522f73e593555976be919cef048b4a2c570a7
highjoon <yundosa2@gmail.com>
2022-02-13 05:31:25 +0900

    3273 두 수의 합

---
+++ BOJ/투포인터/3273.js
@@ -0,0 +1,29 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = Number(input.shift());
+const nums = input
+  .shift()
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+const x = Number(input.shift());
+
+let left = 0;
+let right = n - 1;
+let answer = 0;
+
+while (left < right) {
+  let sum = nums[left] + nums[right];
+
+  if (sum === x) {
+    answer++;
+    left++;
+  } else if (sum < x) {
+    left++;
+  } else {
+    right--;
+  }
+}
+
+console.log(answer);
