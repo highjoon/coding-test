cc97f59ade6c52d9d4f5cfe567f4cdfa50b781b2
highjoon <yundosa2@gmail.com>
2022-02-13 01:09:11 +0900

    2003 수들의 합 2

---
+++ BOJ/투포인터/2003.js
@@ -0,0 +1,29 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [N, M] = input
+  .shift()
+  .split(" ")
+  .map((v) => Number(v));
+const nums = input
+  .shift()
+  .split(" ")
+  .map((v) => Number(v));
+
+let answer = 0;
+let left = 0;
+let right = 0;
+let sum = nums[left];
+
+while (left < N && right < N) {
+  if (sum === M) {
+    answer++;
+    sum += nums[++right];
+  } else if (sum < M) {
+    sum += nums[++right];
+  } else if (sum > M) {
+    sum -= nums[left++];
+  }
+}
+
+console.log(answer);
