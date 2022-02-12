6ed666ed0e05d6db4d459fc687d3de1b44e51cb0
highjoon <yundosa2@gmail.com>
2022-02-13 01:45:42 +0900

    11728 배열 합치기

---
+++ BOJ/투포인터/11728.js
@@ -0,0 +1,41 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [N, M] = input
+  .shift()
+  .split(" ")
+  .map((v) => Number(v));
+
+const A = input
+  .shift()
+  .split(" ")
+  .map((v) => Number(v));
+
+const B = input
+  .shift()
+  .split(" ")
+  .map((v) => Number(v));
+
+let idxA = 0;
+let idxB = 0;
+let answer = [];
+
+while (idxA < N && idxB < M) {
+  if (A[idxA] === B[idxB]) {
+    answer.push(A[idxA]);
+    answer.push(B[idxB]);
+    idxA++;
+    idxB++;
+  } else if (A[idxA] < B[idxB]) {
+    answer.push(A[idxA]);
+    idxA++;
+  } else {
+    answer.push(B[idxB]);
+    idxB++;
+  }
+}
+
+answer = answer.concat(A.slice(idxA));
+answer = answer.concat(B.slice(idxB));
+
+console.log(answer.join(" "));
