d0e9bb5b57ca648a434b23b51c83426bfb7cd5cd
highjoon <yundosa2@gmail.com>
2022-02-15 19:16:32 +0900

    1300 K번째 수

---
+++ BOJ/이분탐색/1300.js
@@ -0,0 +1,37 @@
+let fs = require("fs");
+let input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const [N, k] = input;
+
+let left = 1;
+let right = k;
+let answer = 0;
+
+function countOrder(mid) {
+  let cnt = 0;
+
+  for (let i = 1; i <= N; i++) {
+    cnt += Math.min(parseInt(mid / i), N);
+  }
+
+  return cnt;
+}
+
+while (left <= right) {
+  let mid = parseInt((left + right) / 2);
+  let count = countOrder(mid);
+
+  if (count >= k) {
+    answer = mid;
+    right = mid - 1;
+  } else {
+    left = mid + 1;
+  }
+}
+
+console.log(answer);
