f861e6a9b818987572bba39d1d3a76e254fc1f7d
highjoon <yundosa2@gmail.com>
2022-02-24 12:51:39 +0900

    징검다리

---
+++ Programmers/이분탐색/징검다리.js
@@ -0,0 +1,25 @@
+function solution(distance, rocks, n) {
+  let left = 0;
+  let right = distance;
+  let answer = Number.MIN_SAFE_INTEGER;
+  rocks = rocks.sort((a, b) => a - b);
+
+  while (left <= right) {
+    let mid = parseInt((left + right) / 2);
+    let count = 0;
+    let current = 0;
+
+    for (let i = 0; i < rocks.length; i++) {
+      if (rocks[i] - current <= mid) count++;
+      else current = rocks[i];
+    }
+
+    if (count > n) right = mid - 1;
+    else left = mid + 1;
+  }
+
+  answer = Math.max(answer, left, right);
+  return answer;
+}
+
+console.log(solution(25, [2, 14, 11, 21, 17], 2));
