05a2305bdcd872daeda6e8740d1f198a57672fab
highjoon <yundosa2@gmail.com>
2022-03-23 22:20:59 +0900

    7-2. 버블정렬 (Re)

---
+++ JS_Algorithm/Class/Section07/2.버블정렬_문제.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < answer.length; i++) {
+    for (let j = 0; j < answer.length - i - 1; j++) {
+      if (answer[j] > answer[j + 1]) {
+        [answer[j + 1], answer[j]] = [answer[j], answer[j + 1]];
+      }
+    }
+  }
+  return answer;
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
