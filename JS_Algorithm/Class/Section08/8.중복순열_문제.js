4bd0267aa90d02747f04d45dcffbd128e7002d16
highjoon <yundosa2@gmail.com>
2022-04-13 23:13:18 +0900

    8-8. 중복순열 구하기 (Re)

---
+++ JS_Algorithm/Class/Section08/8.중복순열_문제.js
@@ -0,0 +1,19 @@
+function solution(n, m) {
+  const answer = [];
+  let tmp = Array.from({ length: m }, () => 0);
+  function DFS(L) {
+    if (L === m) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 1; i <= n; i++) {
+        tmp[L] = i;
+        DFS(L + 1);
+      }
+    }
+  }
+  DFS(0);
+  answer.push(answer.length);
+  return answer.join("\n");
+}
+
+console.log(solution(3, 2));
