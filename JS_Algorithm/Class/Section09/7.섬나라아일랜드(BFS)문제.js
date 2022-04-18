ac351b9df1457ee31b1c5987c9b6c404891a51db
highjoon <yundosa2@gmail.com>
2022-04-18 21:19:45 +0900

    9-7. 섬나라 아일랜드 (BFS)

---
+++ JS_Algorithm/Class/Section09/7.섬나라아일랜드(BFS)문제.js
@@ -0,0 +1,42 @@
+function solution(board) {
+  let answer = 0;
+  const n = board.length;
+  const queue = [];
+  const dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  const dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        queue.push([i, j]);
+        answer++;
+        while (queue.length) {
+          const [x, y] = queue.shift();
+          for (let i = 0; i < dx.length; i++) {
+            const nx = x + dx[i];
+            const ny = y + dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+              board[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return answer;
+}
+
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(solution(arr));
