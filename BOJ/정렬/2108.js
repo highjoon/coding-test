005396523003f22da19328728613fd26dba007d9
highjoon <yundosa2@gmail.com>
2022-04-20 01:50:59 +0900

    2108. 통계학 (재채점)

--- BOJ/정렬/2108.js
+++ BOJ/정렬/2108.js
@@ -6,40 +6,38 @@ let input = fs
   .split("\n")
   .map((num) => parseInt(num));
 
+// let input = [5, 1, 3, 8, -2, 2];
+// let input = [1, 4000];
+// let input = [5, -1, -2, -3, -1, -2];
+// let input = [3, 0, 0, -1];
+// let input = [1, 0];
+// let input = [3, -2, -4, -8];
+
 const N = Number(input.shift());
 const sortedNumArr = input.sort((a, b) => Number(a) - Number(b));
-const numMap = {};
 
-for (let num of sortedNumArr) {
-  if (numMap[num]) {
-    numMap[num] += 1;
-  } else {
-    numMap[num] = 1;
-  }
+const avg = Math.round(sortedNumArr.reduce((a, b) => a + b, 0) / N);
+const mid = sortedNumArr[parseInt(sortedNumArr.length / 2)];
+let min;
+const range = sortedNumArr[sortedNumArr.length - 1] - sortedNumArr[0];
+
+let sh = new Map();
+
+for (let x of sortedNumArr.reverse()) {
+  if (sh.has(x)) sh.set(x, sh.get(x) + 1);
+  else sh.set(x, 1);
 }
 
-let hitMaxNum = Math.max.apply(null, Object.values(numMap));
-let hitMaxNumArr = [];
-let hitMaxNumResult = 0;
-
-for (let numKey in numMap) {
-  if (numMap[numKey] === hitMaxNum) {
-    hitMaxNumArr.push(numKey);
-  }
+let maxNum = Math.max(...Array.from(sh.values()));
+const temp = [];
+for (let [key, value] of sh) {
+  if (value === maxNum) temp.push(key);
 }
+temp.sort((a, b) => a - b);
+if (temp.length > 1) min = temp[1];
+else min = temp[0];
 
-if (hitMaxNumArr.length > 1) {
-  hitMaxNumArr = hitMaxNumArr.sort((a, b) => a - b);
-  hitMaxNumResult = hitMaxNumArr[1];
-} else {
-  hitMaxNumResult = hitMaxNumArr[0];
-}
-
-const avg = Math.round(input.reduce((acc, num) => (acc += num), 0) / N);
-const center = input[Math.floor(input.length / 2)];
-const maxSubMin = sortedNumArr[sortedNumArr.length - 1] - sortedNumArr[0];
-
-console.log(avg);
-console.log(center);
-console.log(hitMaxNumResult);
-console.log(maxSubMin);
+console.log(avg === -0 ? 0 : avg);
+console.log(mid);
+console.log(min);
+console.log(range);
