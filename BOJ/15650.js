3dda213463140fad121141fc7c9f8e46b5666d99
highjoon <yundosa2@gmail.com>
2022-04-20 03:01:00 +0900

    15650. N과 M (2)

---
+++ BOJ/15650.js
@@ -0,0 +1,32 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const N = Number(input[0]);
+const M = Number(input[1]);
+
+let answer = [];
+let temp = Array.from({ length: M }, () => 0);
+let ch = Array.from({ length: N + 1 }, () => 0);
+
+function DFS(L) {
+  if (L === M) {
+    let slicedTemp = temp.slice();
+    let sortedTemp = temp.slice().sort((a, b) => a - b);
+    if (slicedTemp.toString() === sortedTemp.toString()) {
+      answer.push(temp.slice().join(" "));
+    }
+  } else {
+    for (let i = 1; i <= N; i++) {
+      if (ch[i] === 0) {
+        ch[i] = 1;
+        temp[L] = i;
+        DFS(L + 1);
+        ch[i] = 0;
+      }
+    }
+  }
+}
+
+DFS(0);
+answer = answer.join("\n");
+console.log(answer);
