c372a16e955c80844edf7d786ecb9331e07d14cb
highjoon <yundosa2@gmail.com>
2022-04-24 07:19:45 +0900

    자릿수 더하기 (Re)

---
+++ Programmers/자릿수더하기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  const answer = n
+    .toString()
+    .split("")
+    .reduce((a, b) => Number(a) + Number(b), 0);
+  return answer;
+}
+
+console.log(solution(123));
+console.log(solution(987));
