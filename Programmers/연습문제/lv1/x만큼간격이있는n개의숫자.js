4290a1043e7faa587a7078939fa167e8bedb718d
highjoon <yundosa2@gmail.com>
2022-04-23 11:44:51 +0900

    x만큼 간격이 있는 n개의 숫자 (Re)

---
+++ Programmers/연습문제/lv1/x만큼간격이있는n개의숫자.js
@@ -0,0 +1,8 @@
+function solution(x, n) {
+  const answer = Array.from({ length: n }, () => x).map((v, i) => (v += i * x));
+  return answer;
+}
+
+console.log(solution(2, 5));
+console.log(solution(4, 3));
+console.log(solution(-4, 2));
