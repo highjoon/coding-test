040a9ee8443ca9e50b84fd2adab18a894893ec20
highjoon <yundosa2@gmail.com>
2022-04-24 07:14:34 +0900

    정수 제곱근 판별 (Re)

---
+++ Programmers/정수제곱근판별.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let answer = 0;
+  let s = Math.sqrt(n);
+  if (Number.isInteger(s)) {
+    answer = Math.pow(s + 1, 2);
+  } else {
+    return -1;
+  }
+  return answer;
+}
+
+console.log(solution(121));
+console.log(solution(3));
