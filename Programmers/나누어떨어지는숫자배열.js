c41f047984e3e6b884ced6ba4e6a79a4a227c187
highjoon <yundosa2@gmail.com>
2022-04-24 21:13:45 +0900

    나누어 떨어지는 숫자 배열 (Re)

---
+++ Programmers/나누어떨어지는숫자배열.js
@@ -0,0 +1,9 @@
+function solution(arr, divisor) {
+  const answer = arr.filter((v) => v % divisor === 0).sort((a, b) => a - b);
+  if (!answer.length) answer.push(-1);
+  return answer;
+}
+
+console.log(solution([5, 9, 7, 10], 5));
+console.log(solution([2, 36, 1, 3], 1));
+console.log(solution([3, 2, 6], 10));
