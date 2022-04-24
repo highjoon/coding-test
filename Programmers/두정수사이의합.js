7e0f8467eca9f22701d0e9bb828669cc3f2c7ac2
highjoon <yundosa2@gmail.com>
2022-04-24 12:17:25 +0900

    두 정수 사이의 합 (Re)

---
+++ Programmers/두정수사이의합.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+  let answer = 0;
+  const start = Math.min(a, b);
+  const end = Math.max(a, b);
+  for (let i = start; i <= end; i++) {
+    answer += i;
+  }
+  return answer;
+}
+
+console.log(solution(3, 5));
+console.log(solution(3, 3));
+console.log(solution(5, 3));
