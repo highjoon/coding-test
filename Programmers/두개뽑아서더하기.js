98f3578ecb55ef862c36bea4e960a4f11690e3a0
highjoon <yundosa2@gmail.com>
2022-04-25 13:08:23 +0900

    두 개 뽑아서 더하기 (Re)

---
+++ Programmers/두개뽑아서더하기.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+  const set = new Set();
+  const n = numbers.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === j) continue;
+      set.add(numbers[i] + numbers[j]);
+    }
+  }
+  let answer = Array.from(set).sort((a, b) => a - b);
+  return answer;
+}
+
+console.log(solution([2, 1, 3, 4, 1]));
+console.log(solution([5, 0, 2, 7]));
