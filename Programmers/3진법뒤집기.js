33f686f5db1316c7ed86164ae4aacdb7a2e703d6
highjoon <yundosa2@gmail.com>
2022-04-25 14:41:25 +0900

    3진법 뒤집기 (Re)

---
+++ Programmers/3진법뒤집기.js
@@ -0,0 +1,23 @@
+function solution(n) {
+  let answer = 0;
+  let temp = [];
+
+  function DFS(num) {
+    if (num === 0) return;
+    DFS(parseInt(num / 3));
+    temp.push(num % 3);
+  }
+
+  DFS(n);
+  temp = temp.reverse().join("");
+  console.log(temp);
+
+  for (let i = 0; i < temp.length; i++) {
+    answer += temp[i] * Math.pow(3, temp.length - i - 1);
+  }
+
+  return answer;
+}
+
+console.log(solution(45));
+console.log(solution(125));
