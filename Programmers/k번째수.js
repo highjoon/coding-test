76e445aa7988d2e876c72dcd5fb1ed956a47b7a5
highjoon <yundosa2@gmail.com>
2022-04-25 21:40:49 +0900

    K번째수 (Re)

---
+++ Programmers/k번째수.js
@@ -0,0 +1,24 @@
+function solution(array, commands) {
+  const answer = [];
+  let temp = 0;
+  let start = 0;
+  let end = 0;
+  let target = 0;
+
+  for (let command of commands) {
+    [start, end, target] = command;
+    temp = array.slice(start - 1, end).sort((a, b) => a - b)[target - 1];
+    answer.push(temp);
+  }
+
+  return answer;
+}
+
+let array = [1, 5, 2, 6, 3, 7, 4];
+let commands = [
+  [2, 5, 3],
+  [4, 4, 1],
+  [1, 7, 3],
+];
+
+console.log(solution(array, commands));
