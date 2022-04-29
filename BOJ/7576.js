686b6409a663f68b2f59124679c3895c8c6e6fbd
highjoon <yundosa2@gmail.com>
2022-04-29 11:02:14 +0900

    7576. 토마토 (Re)

--- BOJ/7576.js
+++ BOJ/7576.js
@@ -1,38 +1,14 @@
-// let fs = require("fs");
-// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-// let input = ["6 4", "0 0 0 0 0 0", "0 0 0 0 0 0", "0 0 0 0 0 0", "0 0 0 0 0 1"];
-// let input = [
-//   "6 4",
-//   "0 -1 0 0 0 0",
-//   "-1 0 0 0 0 0",
-//   "0 0 0 0 0 0",
-//   "0 0 0 0 0 1",
-// ];
-// let input = [
-//   "6 4",
-//   "1 -1 0 0 0 0",
-//   "0 -1 0 0 0 0",
-//   "0 0 0 0 -1 0",
-//   "0 0 0 0 -1 1",
-// ];
-// let input = [
-//   "5 5",
-//   "-1 1 0 0 0",
-//   "0 -1 -1 -1 0",
-//   "0 -1 -1 -1 0",
-//   "0 -1 -1 -1 0",
-//   "0 0 0 0 0",
-// ];
-let input = ["2 2", "1 -1", "-1 1"];
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let [M, N] = input[0].split(" ").map(Number);
 let box = input.slice(1).map((v) => v.split(" ").map(Number));
 let ch = Array.from({ length: 1001 }, () => Array(1001).fill(0));
-let answer = 0;
-const queue = [];
+const dx = [1, 0, -1, 0];
+const dy = [0, 1, 0, -1];
 
-const dx = [0, 1, 0, -1];
-const dy = [1, 0, -1, 0];
+const queue = [];
+let answer = 0;
 
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M; j++) {
@@ -42,17 +18,16 @@ for (let i = 0; i < N; i++) {
   }
 }
 
-let day = 0;
 let idx = 0;
+let day = 0;
 
 while (idx !== queue.length) {
   let [y, x, cnt] = queue[idx];
   for (let i = 0; i < dx.length; i++) {
-    const nx = x + dx[i];
-    const ny = y + dy[i];
+    const [nx, ny] = [x + dx[i], y + dy[i]];
     if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
       if (box[ny][nx] === 0) {
-        box[ny][nx] = 1;
+        box[ny][nx] = box[y][x] + 1;
         queue.push([ny, nx, cnt + 1]);
       }
     }
