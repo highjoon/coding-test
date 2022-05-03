8c15973a4302c4d9687a5bad4e3409cebdfda336
highjoon <yundosa2@gmail.com>
2022-05-03 18:53:56 +0900

    11279. 최대 힙 (수정)

--- BOJ/11279.js
+++ BOJ/11279.js
@@ -1,3 +1,11 @@
+let fs = require("fs");
+let input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
 class MaxBinaryHeap {
   constructor() {
     this.values = [];
