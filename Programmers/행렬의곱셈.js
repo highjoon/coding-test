c7c4fb002da6c0d205fff9472e41787d05527c16
highjoon <yundosa2@gmail.com>
2022-05-05 14:35:41 +0900

    행렬의 곱셈 (Re)

---
+++ Programmers/행렬의곱셈.js
@@ -0,0 +1,43 @@
+function solution(arr1, arr2) {
+  let row = arr1.length;
+  let column = arr2[0].length;
+  let answer = Array.from({ length: row }, () => Array(column).fill(0));
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      for (let k = 0; k < arr2.length; k++) {
+        answer[i][j] += arr1[i][k] * arr2[k][j];
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(
+  solution(
+    [
+      [1, 4],
+      [3, 2],
+      [4, 1],
+    ],
+    [
+      [3, 3],
+      [3, 3],
+    ]
+  )
+);
+console.log(
+  solution(
+    [
+      [2, 3, 2],
+      [4, 2, 4],
+      [3, 1, 4],
+    ],
+    [
+      [5, 4, 3],
+      [2, 4, 1],
+      [3, 1, 1],
+    ]
+  )
+);
