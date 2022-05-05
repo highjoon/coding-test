1a2bbcb41a5926ae118b14001a4626e6037d4eac
highjoon <yundosa2@gmail.com>
2022-05-05 16:07:13 +0900

    올바른 괄호 (Re)

---
+++ Programmers/올바른괄호.js
@@ -0,0 +1,19 @@
+function solution(s) {
+  if (s[0] === ")") return false;
+  let answer = true;
+  const stack = [];
+
+  for (let x of s) {
+    if (x === "(") stack.push(x);
+    else stack.pop();
+  }
+
+  if (stack.length) return false;
+  return answer;
+}
+
+console.log(solution("()()"));
+console.log(solution("(())()"));
+console.log(solution(")()("));
+console.log(solution("(()("));
+console.log(solution("))))"));
