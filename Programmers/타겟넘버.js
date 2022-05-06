392c2c61804147b257e6c03de71c6d33465faa26
highjoon <yundosa2@gmail.com>
2022-05-07 00:17:58 +0900

    타겟 넘버

---
+++ Programmers/타겟넘버.js
@@ -0,0 +1,20 @@
+function solution(numbers, target) {
+  let answer = 0;
+
+  function DFS(L, result) {
+    if (L === numbers.length) {
+      if (result === target) {
+        answer++;
+      }
+      return;
+    }
+
+    DFS(L + 1, result + numbers[L]);
+    DFS(L + 1, result - numbers[L]);
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+console.log(solution([1, 1, 1, 1, 1], 3));
+console.log(solution([4, 1, 2, 1], 4));
