a667ef65c77c9555f103da4b477d095eccfad322
highjoon <yundosa2@gmail.com>
2022-05-06 23:53:43 +0900

    124 나라의 숫자

---
+++ Programmers/124나라의숫자.js
@@ -0,0 +1,22 @@
+function solution(n) {
+  let answer = "";
+
+  function calculate(num, result) {
+    if (num === 0) {
+      return result.split("").reverse().join("");
+    } else {
+      if (num % 3 === 0) {
+        return calculate(parseInt(num / 3) - 1, result + "4");
+      } else {
+        return calculate(parseInt(num / 3), result + String(num % 3));
+      }
+    }
+  }
+  answer = calculate(n, "");
+  return answer;
+}
+
+console.log(solution(1));
+console.log(solution(2));
+console.log(solution(3));
+console.log(solution(4));
