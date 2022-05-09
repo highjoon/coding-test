262d0fbfe837f9080cf7085b96927df14408e961
highjoon <yundosa2@gmail.com>
2022-05-09 18:56:36 +0900

    2504. 괄호의 값 (Re)

--- BOJ/2504.js
+++ BOJ/2504.js
@@ -1,80 +1,61 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().trim();
 
-const stack = [];
+function solution(input) {
+  const stack = [];
+  let answer = 0;
 
-function solution(str) {
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === "(" || str[i] === "[") {
-      stack.push(str[i]);
-    } else if (str[i] === ")") {
-      if (stack.length === 0) {
-        return 0;
-      }
+  for (let i = 0; i < input.length; i++) {
+    let current = input[i];
+    if (current === "(" || current === "[") {
+      stack.push(current);
+    } else if (current === ")") {
+      if (stack.length === 0) return 0;
 
       let temp = 0;
 
       while (stack.length) {
         let top = stack.pop();
+
+        if (top === "[") return 0;
 
         if (top === "(") {
-          if (temp !== 0) {
-            stack.push(temp * 2);
-          } else {
-            stack.push(2);
-          }
+          if (temp !== 0) stack.push(temp * 2);
+          else stack.push(2);
           break;
-        } else if (top === "[") {
-          return 0;
         } else {
-          if (temp === 0) {
-            temp = Number(top);
-          } else {
-            temp += Number(top);
-          }
+          if (temp === 0) temp = Number(top);
+          else temp += Number(top);
         }
       }
-    } else if (str[i] === "]") {
-      if (stack.length === 0) {
-        return 0;
-      }
+    } else if (current === "]") {
+      if (stack.length === 0) return 0;
 
       let temp = 0;
 
       while (stack.length) {
         let top = stack.pop();
 
+        if (top === "(") return 0;
+
         if (top === "[") {
-          if (temp !== 0) {
-            stack.push(temp * 3);
-          } else {
-            stack.push(3);
-          }
+          if (temp !== 0) stack.push(temp * 3);
+          else stack.push(3);
           break;
-        } else if (top === "(") {
-          return 0;
         } else {
-          if (temp === 0) {
-            temp = Number(top);
-          } else {
-            temp += Number(top);
-          }
+          if (temp === 0) temp = Number(top);
+          else temp += Number(top);
         }
       }
     }
   }
 
-  let result = 0;
-
   for (let x of stack) {
-    if (x === "(" || x === "[" || x === ")" || x === "]") {
-      return 0;
-    } else {
-      result += Number(x);
-    }
+    if (typeof x === "string") return 0;
+    else answer += x;
   }
 
-  return result;
+  return answer;
 }
 
 console.log(solution(input));
