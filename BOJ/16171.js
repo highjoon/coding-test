cb530b994b1717e19eb8719583fe13778d3bc4a1
highjoon <yundosa2@gmail.com>
2022-05-16 21:04:40 +0900

    16171. 나는 친구가 적다 (Small)

---
+++ BOJ/16171.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = require("fs")
+  .readFileSync("dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(input) {
+  let answer = 0;
+  let S = input[0].trim();
+  let K = input[1].trim();
+  S = S.split("")
+    .filter((v) => isNaN(v))
+    .join("");
+
+  if (S.includes(K)) answer = 1;
+  return answer;
+}
+
+console.log(solution(input));
