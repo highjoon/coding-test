297b3adacff9b0e006508341709e929e83e1b916
highjoon <yundosa2@gmail.com>
2022-05-16 21:34:34 +0900

    6550. 부분 문자열

---
+++ BOJ/6550.js
@@ -0,0 +1,19 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+function solution(input) {
+  const answer = [];
+  for (let words of input) {
+    let [s, t] = words.split(" ").map((v) => v.split(""));
+    s = s.reverse();
+    for (let i = 0; i < t.length; i++) {
+      if (t[i] === s[s.length - 1]) {
+        s.pop();
+      }
+    }
+    answer.push(s.length ? "No" : "Yes");
+  }
+  return answer.join("\n");
+}
+
+console.log(solution(input));
