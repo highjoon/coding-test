c8afeb8d6f302dc576410b4d973cc4196548dd8c
highjoon <yundosa2@gmail.com>
2022-05-26 01:31:25 +0900

    10798. 세로읽기

---
+++ BOJ/10798.js
@@ -0,0 +1,19 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+function solution(input) {
+  const N = Math.max(...input.map((v) => v.length));
+  let answer = "";
+
+  for (let i = 0; i < N; i++) {
+    for (let str of input) {
+      if (str[i]) {
+        answer += str[i];
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(input));
