94ef4b8daadc2d555f7c75e0fb76dbc845b320d2
highjoon <yundosa2@gmail.com>
2022-05-26 14:43:04 +0900

    2204. 도비의 난독증 테스트

---
+++ BOJ/2204.js
@@ -0,0 +1,32 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+function solution(input) {
+  input.pop();
+  input = input.reverse();
+  const answer = [];
+
+  while (input.length) {
+    let n = Number(input.pop());
+    let words = [];
+
+    while (n--) {
+      words.push(input.pop());
+    }
+
+    words.sort((a, b) => {
+      const lowerA = a.toLowerCase();
+      const lowerB = b.toLowerCase();
+
+      if (lowerA > lowerB) return 1;
+      if (lowerA < lowerB) return -1;
+      if (lowerA === lowerB) return 0;
+    });
+
+    answer.push(words[0]);
+  }
+
+  return answer.join("\n");
+}
+
+console.log(solution(input));
