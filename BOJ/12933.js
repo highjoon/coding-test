12936bf0664271020bffdb08ab17e516c5c33375
highjoon <yundosa2@gmail.com>
2022-05-31 02:53:50 +0900

    12933. 오리

---
+++ BOJ/12933.js
@@ -0,0 +1,40 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+function solution(input) {
+  input = input.split("");
+  if (input[0] !== "q" || input[input.length - 1] !== "k") return -1;
+  if (input.length % 5 !== 0) return -1;
+
+  const quack = "quack";
+  let visited = Array.from({ length: input.length }, () => false);
+  let idx = 0;
+  let answer = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === "q" && !visited[i]) {
+      let flag = true;
+      for (let j = i; j < input.length; j++) {
+        if (input[j] === quack[idx] && !visited[j]) {
+          idx++;
+          visited[j] = true;
+        }
+        if (idx === 5) {
+          if (flag) {
+            answer++;
+            flag = false;
+          }
+          idx = 0;
+          continue;
+        }
+      }
+    }
+  }
+
+  for (let v of visited) {
+    if (v === false) return -1;
+  }
+  return answer > 0 ? answer : -1;
+}
+
+console.log(solution(input));
---
+++ BOJ/9536.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+function solution(input) {
+  input.pop();
+  input = input.reverse();
+  let T = Number(input.pop());
+  const answer = [];
+
+  while (T > 0) {
+    let record = input.pop().split(" ");
+    let voices = [];
+    while (input.length && input[input.length - 1].split(" ")[1] === "goes") {
+      voices.push(input.pop());
+    }
+    voices = voices.map((v) => v.split(" ").pop());
+    let result = record.filter((v) => !voices.includes(v));
+    T--;
+    answer.push(result.join(" "));
+    input.pop();
+  }
+
+  return answer.join("\n");
+}
+
+console.log(solution(input));
