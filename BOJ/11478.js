b8ac4fa0996f25eb37aaf220369c0a2ccdd84451
highjoon <yundosa2@gmail.com>
2022-06-03 01:11:39 +0900

    11478. 서로 다른 부분 문자열의 개수

---
+++ BOJ/11478.js
@@ -0,0 +1,18 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+function solution(input) {
+  let answer = 0;
+  let set = new Set();
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i; j < input.length; j++) {
+      set.add(input.substring(i, j + 1));
+    }
+  }
+
+  answer = set.size;
+  return answer;
+}
+
+console.log(solution(input));
