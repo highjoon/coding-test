f7d751e463dc6c0aa463b0b3b8fb108e93db7a0c
highjoon <yundosa2@gmail.com>
2022-06-22 00:15:16 +0900

    2729. 이진수 덧셈

---
+++ BOJ/2729.js
@@ -0,0 +1,18 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+function solution(input) {
+  let T = Number(input[0]);
+  const answer = [];
+  input = input.slice(1).map((v) => v.split(" "));
+
+  for (let i = 0; i < T; i++) {
+    let [a, b] = input[i];
+    let sum = BigInt("0b" + a) + BigInt("0b" + b);
+    answer.push(sum.toString(2));
+  }
+
+  return answer.join("\n");
+}
+
+console.log(solution(input));
