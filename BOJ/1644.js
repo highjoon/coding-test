2c15c1f70992d6d362bd36daa9eecce79fe04359
highjoon <yundosa2@gmail.com>
2022-07-02 01:06:34 +0900

    1644. 소수의 연속합 (Re)

--- BOJ/1644.js
+++ BOJ/1644.js
@@ -1,55 +1,47 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().trim();
 
-const N = Number(input);
-let primes = [];
+function solution(input) {
+    const N = Number(input);
+    let primes = [];
+    let left = 0;
+    let right = 1;
+    let answer = 0;
+    let sum = 0;
 
-function eratosthenesSieve(array, number) {
-    let tempArray = [];
+    function eratosthenesSieve(number) {
+        const array = [];
+        const tempArray = [];
 
-    for (let i = 2; i <= number; i++) {
-        tempArray[i] = i;
-    }
+        for (let i = 2; i <= number; i++) tempArray[i] = i;
 
-    for (let i = 2; i <= number; i++) {
-        if (tempArray[i] === 0) {
-            continue;
+        for (let i = 2; i <= number; i++) {
+            if (tempArray[i] === 0) continue;
+            for (let j = i + i; j <= number; j += i) tempArray[j] = 0;
         }
 
-        for (let j = i + i; j <= number; j += i) {
-            tempArray[j] = 0;
+        for (let i = 2; i <= number; i++) {
+            if (tempArray[i] !== 0) array.push(tempArray[i]);
+        }
+
+        return array;
+    }
+
+    primes = eratosthenesSieve(N);
+    sum = primes[left];
+
+    while (left < primes.length && right < primes.length) {
+        if (sum <= N) {
+            if (sum === N) answer++;
+            sum += primes[right++];
+        } else {
+            sum -= primes[left++];
         }
     }
 
-    for (let i = 2; i <= number; i++) {
-        if (tempArray[i] !== 0) {
-            array.push(tempArray[i]);
-        }
-    }
+    if (primes[primes.length - 1] === N) answer++;
 
-    return array;
+    return answer;
 }
 
-eratosthenesSieve(primes, N);
-
-let left = 0;
-let right = 1;
-let sum = primes[left];
-let answer = 0;
-
-while (left < primes.length && right < primes.length) {
-    if (sum === N) {
-        answer++;
-        sum += primes[right++];
-    } else if (sum > N) {
-        sum -= primes[left++];
-    } else {
-        sum += primes[right++];
-    }
-}
-
-if (primes[primes.length - 1] === N) {
-    answer++;
-}
-
-console.log(answer);
+console.log(solution(input));
