8ee51898a054c0f47eadc8a74812ec0d21392f44
highjoon <yundosa2@gmail.com>
2022-07-07 23:16:57 +0900

    2800. 괄호 제거

---
+++ BOJ/2800.js
@@ -0,0 +1,45 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+function getCombinations(arr, selectNumber) {
+    const results = [];
+    if (selectNumber === 1) return arr.map((el) => [el]);
+
+    arr.forEach((fixed, index, origin) => {
+        const rest = origin.slice(index + 1);
+        const combinations = getCombinations(rest, selectNumber - 1);
+        const attached = combinations.map((el) => [fixed, ...el]);
+        results.push(...attached);
+    });
+
+    return results;
+}
+
+function solution(input) {
+    input = input.split("");
+    const li = [];
+    const stack = [];
+    const answer = new Set();
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === "(") stack.push(i);
+        else if (input[i] === ")") li.push([stack.pop(), i]);
+    }
+
+    for (let i = 0; i < li.length + 1; i++) {
+        const combinations = getCombinations(li, i);
+
+        for (let combination of combinations) {
+            let temp = input.slice();
+            for (let combi of combination) {
+                temp[combi[0]] = "";
+                temp[combi[1]] = "";
+            }
+            answer.add(temp.join(""));
+        }
+    }
+
+    return Array.from(answer).sort().join("\n");
+}
+
+console.log(solution(input));
